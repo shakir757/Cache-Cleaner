db0fff7c09bdab2466a3291fe7d75214534bc59f
shakir757 <shakir757@mail.ru>
2023-01-23 12:39:26 +0300

    Activities created

---
+++ app/src/main/java/com/cache/cleaner/start/app/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cache.cleaner.start.app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cache.cleaner.start.app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
