c3f76e8c91fc71a514991c0c962493e36e5b6aff
mirsaitsabirzanov <mirsait77777@gmail.com>
2023-01-26 10:36:49 +0300

    add needed files

---
+++ app/src/main/java/com/cache/cleaner/start/app/CacheFragment.java
@@ -0,0 +1,31 @@
+package com.cache.cleaner.start.app;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Date;
+
+
+public class CacheFragment extends Fragment {
+
+
+    public CacheFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_cache, container, false);
+    }
+
+}
